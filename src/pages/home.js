ea97c8fb8a24c97d451f3238f341d06ccb1551a0
Juan Camilo Losada <juancamilo.lb@hotmail.com>
2022-01-11 22:13:05 -0500

    Updates

---
+++ src/pages/home.js
@@ -0,0 +1,20 @@
+import getData from "../utils/getData";
+
+const Home = async () => {
+    const characters = await getData();
+    const view = /*html*/`
+    <div class="Characters">
+        ${characters.results.map(character => /*html*/ `
+            <article class="Character-item">
+                <a href="#/${character.id}/">
+                    <img src="${character.image}" alt="${character.name}">
+                    <h2>${character.name}</h2>
+                </a>
+            </article>
+        `).join('')}
+    </div>
+    `;
+    return view;
+};
+
+export default Home;
